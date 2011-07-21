2d9cb29f10a017536301bdc5f107aa4d03e885d5
Johannes Barre <igel@igels.net>
2011-07-21 23:03:32 +0200

    Tag#process implemented

--- lib/it/tag.rb
+++ lib/it/tag.rb
@@ -1,12 +1,22 @@
 module It
   class Tag
-    attr_reader :tag, :options
+    include ActionView::Helpers::TagHelper
     
-    def initialize(tag, options = {})
-      raise TypeError, "Tag must be specified as a String or Symbol" unless tag.is_a?(String) || tag.is_a?(Symbol)
-      raise TypeError, "Options must be specified as a Hash" unless options.is_a?(Hash)
-      @tag = tag.to_sym
+    attr_reader :tag_name, :options
+    
+    def initialize(tag_name, options = {})
+      raise TypeError, "tag_name must be specified as a String or Symbol" unless tag_name.is_a?(String) || tag_name.is_a?(Symbol)
+      raise TypeError, "options must be specified as a Hash" unless options.is_a?(Hash)
+      @tag_name = tag_name.to_sym
       @options = options.symbolize_keys
+    end
+    
+    def process(content = nil)
+      if content.nil?
+        tag(@tag_name, @options)
+      else
+        content_tag(@tag_name, content, @options)
+      end
     end
   end
 end
--- spec/it/tag_spec.rb
+++ spec/it/tag_spec.rb
@@ -29,18 +29,40 @@ describe It::Tag, '.new' do
   end
 end
 
-describe It::Tag, '#tag' do
+describe It::Tag, '#tag_name' do
   it "should return the tag as a Symbol if given as a Symbol" do
-    It::Tag.new(:i).tag.should == :i
+    It::Tag.new(:i).tag_name.should == :i
   end
   
-  it "should return the tag as a Symbol if given as a String" do
-    It::Tag.new("i").tag.should == :i
+  it "should return the tag_name as a Symbol if given as a String" do
+    It::Tag.new("i").tag_name.should == :i
   end
 end
 
 describe It::Tag, '#options' do
   it "should return the options with symbolized keys" do
     It::Tag.new(:i, "id" => "cool", :class => "classy").options.should == {id: "cool", class: "classy"}
+  end
+end
+
+describe It::Tag, '#process' do
+  it "should return a tag with the options as attributes and the param as content" do
+    It::Tag.new(:i, "id" => "cool", "class" => "classy").process("some text").should == '<i class="classy" id="cool">some text</i>'
+  end
+  
+  it "should be marked as html safe" do
+    It::Tag.new(:i).process("some text").html_safe.should be_true
+  end
+  
+  it "should escape HTML" do
+    It::Tag.new(:i).process("some text & <b>html</b>").should == '<i>some text &amp; &lt;b&gt;html&lt;/b&gt;</i>'
+  end
+  
+  it "should not escape strings marked as HTML safe" do
+    It::Tag.new(:i).process("some text & <b>html</b>".html_safe).should == '<i>some text & <b>html</b></i>'
+  end
+  
+  it "should return an empty tag, if no content is provided" do
+    It::Tag.new(:br, "id" => "cool").process.should == '<br id="cool" />'
   end
 end
