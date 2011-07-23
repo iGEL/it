834a381ce16220f878645808b91ae6a717342245
Johannes Barre <igel@igels.net>
2011-07-23 11:56:01 +0200

    Old t_link helper converted

--- lib/it/helper.rb
+++ lib/it/helper.rb
@@ -2,42 +2,22 @@
 
 module It
   module Helper
-    include ActionView::Helpers::UrlHelper
-    include ERB::Util
-    
-    def t_tags(translation, options = {})
-      options.symbolize_keys!
-      string = String.new(h(t(translation))) # We want an escaped String, not an ActiveSupport::SafeBuffer
-
-      # Resolve the inner interpolations of nested interpolations
-      string.gsub!(/(%\{[^}]+:[^}]*)(%\{[^}]+\})([^}]*\})/) do |tl|
-        token = $2[2..-2].to_sym
-        if options.has_key?(token)
-          "#{$1}#{options[token]}#{$3}"
-        else
-          tl
-        end
-      end
+    def it(identifier, options = {})
+      options.stringify_keys!
+      translation = String.new(h(t(identifier)))
       
-      # Parse links
-      string.gsub!(/%\{[^}]+\}/) do |tl|
-        if tl.include?(":")
-          token, label = tl[2..-2].split(":", 2)
-          addr = options.delete(token.to_sym)
-          if addr.nil?
-            tl
+      while translation =~ /%\{[^{}}]+\}/
+        translation.gsub!(/%\{[^{}}]+\}/) do |interpolation|
+          token, label = interpolation[2..-2].split(":", 2)
+          if label
+            options[token].process(raw label)
           else
-            link_options = options.delete("#{token}_options".to_sym)
-            link_to(raw(label), addr, link_options)
+            h(options[token])
           end
-        else
-          tl
         end
       end
       
-      # Escape HTML of the replacements and interpolate
-      options.each { |key, value| options[key] = (value.is_a?(String) && !value.html_safe?) ? h(value) : value }
-      raw(string % options)
+      raw translation
     end
   end
 end
--- spec/it/helper_spec.rb
+++ spec/it/helper_spec.rb
@@ -3,3 +3,68 @@
 require 'spec_helper'
 require 'it'
 
+describe It::Helper, "#it" do
+  before do
+    I18n.backend.store_translations(:en, :test1 => "I'm containing a %{link:link to Rails} in the middle.")
+    I18n.backend.store_translations(:de, :test1 => "Ich enthalte einen %{link:Link zu Rails} in der Mitte.")
+    
+    @view = ActionView::Base.new
+    @controller = ActionController::Base.new
+    @view.controller = @controller
+  end
+  
+  after do
+    I18n.locale = :en
+  end
+  
+  it "should insert the link into the string" do
+    @view.it("test1", :link => It.link("http://www.rubyonrails.org")).should == 'I\'m containing a <a href="http://www.rubyonrails.org">link to Rails</a> in the middle.'
+  end
+  
+  it "should insert the link into the German translation" do
+    I18n.locale = :de
+    @view.it("test1", :link => It.link("http://www.rubyonrails.org")).should == 'Ich enthalte einen <a href="http://www.rubyonrails.org">Link zu Rails</a> in der Mitte.'
+  end
+  
+  it "should allow link options to be set" do
+    @view.it("test1", :link => It.link("http://www.rubyonrails.org", :target => "_blank")).should == 'I\'m containing a <a href="http://www.rubyonrails.org" target="_blank">link to Rails</a> in the middle.'
+  end
+  
+  it "should parse other tags as well" do
+    @view.it("test1", :link => It.tag(:b, :class => "classy")).should == 'I\'m containing a <b class="classy">link to Rails</b> in the middle.'
+  end
+  
+  it "should mark the result as html safe" do
+    @view.it("test1", :link => It.link("http://www.rubyonrails.org")).html_safe?.should be_true
+  end
+  
+  it "should escape all html in the translation" do
+    I18n.backend.store_translations(:en, :test2 => "<a href=\"hax0r\"> & %{link:link -> Rails} in <b>the middle</b>.")
+    @view.it("test2", :link => It.link("http://www.rubyonrails.org")).should == '&lt;a href=&quot;hax0r&quot;&gt; &amp; <a href="http://www.rubyonrails.org">link -&gt; Rails</a> in &lt;b&gt;the middle&lt;/b&gt;.'
+  end
+  
+  it "should also work with 2 links" do
+    I18n.backend.store_translations(:en, :test3 => "I like %{link1:rails} and %{link2:github}.")
+    @view.it("test3", :link1 => It.link("http://www.rubyonrails.org"), :link2 => It.link("http://www.github.com")).should == 'I like <a href="http://www.rubyonrails.org">rails</a> and <a href="http://www.github.com">github</a>.'
+  end
+  
+  it "should allow normal I18n interpolations" do
+    I18n.backend.store_translations(:en, :test4 => "I'm containing a %{link:link to Rails} in the %{position}.")
+    @view.it("test4", :link => It.link("http://www.rubyonrails.org"), :position => "middle").should == 'I\'m containing a <a href="http://www.rubyonrails.org">link to Rails</a> in the middle.'
+  end
+  
+  it "should escape the HTML in normal interpolations" do
+    I18n.backend.store_translations(:en, :test5 => "Hello %{name}.")
+    @view.it("test5", :name => '<a href="http://evil.haxor.com">victim</a>').should == 'Hello &lt;a href=&quot;http://evil.haxor.com&quot;&gt;victim&lt;/a&gt;.'
+  end
+  
+  it "should not escape html_safe interpolations" do
+    I18n.backend.store_translations(:en, :test5 => "Hello %{name}.")
+    @view.it("test5", :name => '<a href="http://www.rubyonrails.org">Rails</a>'.html_safe).should == 'Hello <a href="http://www.rubyonrails.org">Rails</a>.'
+  end
+  
+  it "should allow interpolations inside of links" do
+    I18n.backend.store_translations(:en, :test6 => "Did you read our %{link:nice %{article}}?")
+    @view.it("test6", :link => It.link("/article/2"), :article => "article").should == 'Did you read our <a href="/article/2">nice article</a>?'
+  end
+end
